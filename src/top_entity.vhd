365fd9d63cf971a6a9349d12e25752055bff0cfc
scp10086 <2632711141@qq.com>
2019-09-02 19:09:47 +0800

    Create top_entity.vhd

---
+++ src/top_entity.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2019/09/02 19:08:29
+-- Design Name: 
+-- Module Name: top_entity - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top_entity is
+--  Port ( );
+end top_entity;
+
+architecture Behavioral of top_entity is
+
+begin
+
+
+end Behavioral;
