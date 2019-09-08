eb2fa6b4ebdc76df8bbce9c139493295dd3e57bd
scp10086 <2632711141@qq.com>
2019-09-08 23:11:08 +0800

    add coe

--- other/changecsvtocoe.py
+++ other/changecsvtocoe.py
@@ -5,6 +5,10 @@ reader = csv.reader(csvFile)
 result = []
 for item in reader:
     for pp in item:
+        pp = int(pp)
+        pp = hex(pp)
+        pp = str(pp)
+        pp = pp[2]
         txtfile.write(pp)
         txtfile.write(',\r')
 csvFile.close() 
